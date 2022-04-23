c0dac304182f510dc6079c10c7afd5bd04b0dfc2
rahim <rahimdev786@gmail.com>
2022-04-23 22:42:39 +0530

    Initial Commit

---
+++ Protocal_UIClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Protocal_UIClass
+//
+//  Created by arshad on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
